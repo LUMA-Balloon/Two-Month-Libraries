e43d0dbcfe152f8d9a2368bb0bcf8feb87f71697
Sa9m <146889719+intentionalDisaster99@users.noreply.github.com>
2025-10-21 22:26:55 -0500

    Added example code

--- LibraryTesting.ino
+++ LibraryTesting.ino
@@ -1,28 +1,44 @@
-#include <BNO055.h>
+#include <SHC_BNO055.h>
 #include <Utils.h>
-#include <BME280.h>
-#include <M9N.h>
+#include <SHC_BME280.h>
+#include <SHC_M9N.h>
 
+/*
+ * This is what the library was tested with. It runs all of the sensors on their default I2C addresses using the default pins and prints to Serial the data.  
+ * The built in LED will blink if there is a wiring error and if the GPS is being tested, it will be on until a lock is achieved. 
+ * Note that this also means that if you have any of the sensors not correctly wired - or not wired at all - then it will lock and not return anything.
+ * With that in mind, comment out any sensor tests and inits for sensors you do not currently have connected. 
+*/
 
-M9N gps = new M9N();
-BNO055 imu = new BNO055();
-M9N gps = new BME280();
+M9N gps = M9N();
+BNO055 mybno = BNO055();
+SHC_BME280 press = SHC_BME280();
 
 void GPStest();
-void GPSInit();
+void GPSinit();
+void BNOtest();
+void BNOinit();
+void BMEtest();
+void BMEinit();
 
 void setup() {
+  Wire.begin();  
+
 
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 
   GPSinit();
-
+  BNOinit();
+  BMEinit();
 
 }
 
 void loop() {
   GPStest();
+  BNOtest();
+  BMEtest();
+  delay(50);
 }
 
 
@@ -33,11 +49,16 @@ void GPSinit() {
     delay(500);
     digitalWrite(LED_BUILTIN, LOW);
     delay(500);
+    Serial.println("M9N Not Found");
   }
 }
-void testGPS() {
+void GPStest() {
+  gps.prefetchData();
   if (gps.getSIV() < 3) {
     digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Waiting for lock; SIV at " + String(gps.getSIV()));
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
   }
   Serial.println("Latitude: " + String(gps.getLatitude()));
   Serial.println("Longitude: " + String(gps.getLongitude()));
@@ -49,5 +70,51 @@ void testGPS() {
   Serial.print(":" + String(gps.getMinute()));
   Serial.println(":" + String(gps.getSecond()));
   Serial.println("SIV: " + String(gps.getSIV()));
+  Serial.println("UNIX: " + String(gps.getUnixTime()));
+
 }
+
+void BNOinit() {
+  while (mybno.init() != NO_ERROR) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    Serial.println("BNO Not Found");
+  }
+}
+
+void BNOtest() {
+  mybno.prefetchData();
+  Serial.print("Accel: (" + String(mybno.getAccelerationX()));
+  Serial.print(", " + String(mybno.getAccelerationY()));
+  Serial.println(", " + String(mybno.getAccelerationZ()) + ")");
+  Serial.print("Gyro: (" + String(mybno.getGyroX()));
+  Serial.print(", " + String(mybno.getGyroY()));
+  Serial.println(", " + String(mybno.getGyroZ()) + ")");
+  Serial.print("Orient: (" + String(mybno.getOrientationX()));
+  Serial.print(", " + String(mybno.getOrientationY()));
+  Serial.println(", " + String(mybno.getOrientationZ()) + ")");
+}
+
+
+void BMEinit() {
+  while (press.init() != NO_ERROR) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    Serial.println("BME Not Found");
+  }
+}
+
+void BMEtest() {
+  press.prefetchData();
+  Serial.println("Pressue: " + String(press.getPressure()));
+  Serial.println("Temperature: " + String(press.getTemperature()));
+  Serial.println("Altitude: " + String(press.getAltitude()));
+  Serial.println("Humidity: " + String(press.getHumidity()));
+  
+}
+
 
